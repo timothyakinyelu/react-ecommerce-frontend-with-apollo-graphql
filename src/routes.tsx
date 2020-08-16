c844feafca3aa9d6b88fa138e2ad1a2a2455be06
loverofMush <tolujamal1@gmail.com>
2020-08-16 17:20:16 +0100

    - open if user logged in commented out - added admin route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,21 +3,44 @@ import { Switch, Route } from 'react-router-dom';
 
 import AdminLogin from './Admin/pages/Auth/AdminLogin';
 import Home from './Shop/pages/Home';
+// import { useQuery, gql } from '@apollo/client';
+import Dashboard from './Admin/pages/Dashboard';
 
 const Routes: React.FC = (): JSX.Element => {
     return (
         <Switch>
             <Route exact path="/" component={ShopComponent} />
-            <Route exact path="/admin" component={AdminComponent} />
+            <Route path="/admin" component={AdminComponent} />
         </Switch>
     );
 };
 
 const AdminComponent = () => {
+    // const IS_LOGGED_IN = gql`
+    //     query IsUserLoggedIn {
+    //         isLoggedIn @client
+    //     }
+    // `;
+
+    // const IsLoggedIn = () => {
+    //     const { data } = useQuery(IS_LOGGED_IN);
+    //     console.log(data);
+    //     return data.isLoggedIn ? (
+    //         <Fragment>
+    //             <Switch>
+    //                 <Route exact path="/admin" component={Dashboard} />
+    //             </Switch>
+    //         </Fragment>
+    //     ) : (
+    //         <Route exact path="/admin/login" component={AdminLogin} />
+    //     );
+    // };
+
     return (
         <Fragment>
+            <Route exact path="/admin/login" component={AdminLogin} />
             <Switch>
-                <Route exact path="/admin" component={AdminLogin} />
+                <Route exact path="/admin" component={Dashboard} />
             </Switch>
         </Fragment>
     );
