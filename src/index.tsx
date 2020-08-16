8ba10bb9c83576f74d810c4cbe504f2e1c6a5abc
loverofMush <tolujamal1@gmail.com>
2020-08-16 17:17:13 +0100

    - import @apollo/client - import typeDefs - import IS_LOGGED_IN - attach authorization header to HttoLink - write query to cache - add ApolloProvider with client

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,40 @@ import './index.css';
 import './tailwind.output.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache, NormalizedCacheObject, ApolloProvider, HttpLink } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { typeDefs } from './graphql/resolvers';
+import { IS_LOGGED_IN } from './graphql/query/auth';
+
+const cache = new InMemoryCache();
+const httpLink = new HttpLink({
+    uri: 'http://localhost:8000/graphql',
+    headers: {
+        authorization: 'Bearer ' + localStorage.getItem('token'),
+    },
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    link: httpLink,
+    typeDefs,
+});
+
+cache.writeQuery({
+    query: IS_LOGGED_IN,
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        user: JSON.parse(localStorage.getItem('user') || '{}'),
+    },
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <ApolloProvider client={client}>
+            <Router>
+                <App />
+            </Router>
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
