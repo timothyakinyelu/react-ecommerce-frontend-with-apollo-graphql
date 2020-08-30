62d68fae9440eb3f070004b2cc84138bd3e64613
loverofMush <tolujamal1@gmail.com>
2020-08-31 00:59:19 +0100

    - future refactoring of routes

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export enum AuthRoutes {
+    home = '/',
+    dashboard = 'dashboard',
+}
+
+export enum NonAuthRoutes {
+    login = 'login',
+    unauthorized = 'unauthorized',
+    notFound = 'not-found',
+}
