59a3fad4d122b675a5b401c46928851bd257006d
loverofMush <tolujamal1@gmail.com>
2020-08-31 00:59:47 +0100

    - pruned

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -5,39 +7,19 @@ import './index.css';
 import './tailwind.output.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloClient, InMemoryCache, NormalizedCacheObject, ApolloProvider, HttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { typeDefs } from './graphql/resolvers';
-import { IS_LOGGED_IN } from './graphql/query/auth';
-
-const cache = new InMemoryCache();
-const httpLink = new HttpLink({
-    uri: 'http://localhost:8000/graphql',
-    headers: {
-        authorization: 'Bearer ' + localStorage.getItem('token'),
-    },
-});
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    cache,
-    link: httpLink,
-    typeDefs,
-});
-
-cache.writeQuery({
-    query: IS_LOGGED_IN,
-    data: {
-        isLoggedIn: !!localStorage.getItem('token'),
-        user: JSON.parse(localStorage.getItem('user') || '{}'),
-    },
-});
+import Auth from './Auth';
+import { client } from './config';
 
 ReactDOM.render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <Router>
-                <App />
-            </Router>
+            <Auth>
+                <Router>
+                    <App />
+                </Router>
+            </Auth>
         </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
