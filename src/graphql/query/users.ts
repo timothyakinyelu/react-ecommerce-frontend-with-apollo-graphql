e92caa847209afc9d7e7807f3e0d7761ceb6a0cc
loverofMush <tolujamal1@gmail.com>
2020-08-31 01:03:42 +0100

    - get users query schema

---
+++ src/graphql/query/users.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const GET_USERS = gql`
+    query GetUsers {
+        users {
+            id
+            first_name
+            last_name
+            email
+        }
+    }
+`;
