559911f7579a0932902558a6815f6ee6437c6f8e
loverofMush <tolujamal1@gmail.com>
2020-09-04 15:32:19 +0100

    - add test route to check auth after token expiry

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,13 +5,15 @@ import Login from './pages/Auth/Login';
 // import { useQuery, gql } from '@apollo/client';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+import Category from './pages/Category';
 
 const Routes: React.FC = (): JSX.Element => {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/categories" component={Category} />
         </Switch>
     );
 };
