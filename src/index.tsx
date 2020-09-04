f323141641453c4511310e7e20d45db307124653
loverofMush <tolujamal1@gmail.com>
2020-09-04 15:31:40 +0100

    refactor file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,23 +5,23 @@ import './index.css';
 import './tailwind.output.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Auth from './Auth';
-import { client } from './config';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <Auth>
-                <Router>
-                    <App />
-                </Router>
-            </Auth>
-        </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+const Root = (): JSX.Element => {
+    return (
+        <React.StrictMode>
+            <Router>
+                <App />
+            </Router>
+        </React.StrictMode>
+    );
+};
+
+const render = () => {
+    ReactDOM.render(<Root />, document.getElementById('root'));
+};
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
