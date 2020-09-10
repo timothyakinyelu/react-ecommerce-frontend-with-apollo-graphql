cef0763cf7fe7b3b6d4cd45af9e7385fa5941775
loverofMush <tolujamal1@gmail.com>
2020-09-10 02:25:27 +0100

    - created navbar header

---
+++ src/components/Navigation/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../../styles/navbar.css';
+import SearchBox from '../../reusables/SearchBox';
+import NotificationIcon from '../../reusables/NotificationIcon';
+import LoggedIcon from '../../reusables/LoggedIcon';
+
+const NavBar: React.FC = (): JSX.Element => {
+    return (
+        <nav className="navbar flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            {/* <div className="block lg:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div> */}
+            <ul className="navbar-icons">
+                <li className="notification-icon">
+                    <NotificationIcon />
+                </li>
+                <li className="search-box">
+                    <SearchBox />
+                </li>
+                <LoggedIcon />
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
