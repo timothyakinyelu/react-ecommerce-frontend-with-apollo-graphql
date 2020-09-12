562cc74f945eecd6cf65a0a04a726a780a7a45e3
loverofMush <tolujamal1@gmail.com>
2020-09-12 03:25:55 +0100

    - addOrRemoveToast mutation to add toasts to cache

--- src/graphql/resolvers.tsx
+++ src/graphql/resolvers.tsx
@@ -1,8 +1,49 @@
-import { gql } from '@apollo/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { gql, ApolloCache, Resolvers } from '@apollo/client';
+import { GET_TOASTS } from './query/toast';
 
 export const typeDefs = gql`
     extend type Query {
         isLoggedIn: Boolean!
         use: User!
+        toasts: Toasts!
+    }
+
+    extend type Mutation {
+        addOrRemoveToast(toast: Toast!): [Toast!]!
     }
 `;
+
+type ResolverFn = (parent: any, args: any, { cache }: { cache: ApolloCache<any> }) => any;
+
+interface ResolverMap {
+    [field: string]: ResolverFn;
+}
+
+interface AppResolvers extends Resolvers {
+    // We will update this with our app's resolvers later
+    Mutation: ResolverMap;
+}
+
+export const resolvers: AppResolvers = {
+    Mutation: {
+        addOrRemoveToast: (_, { toast }: { toast: Toast }, { cache }): Toast[] => {
+            const queryResult = cache.readQuery<ToastState, any>({
+                query: GET_TOASTS,
+            });
+
+            if (queryResult) {
+                const { toasts } = queryResult;
+                const { id } = toast;
+                const data = {
+                    toasts: toasts.includes(toast) ? toasts.filter((i) => i.id !== id) : [...toasts, toast],
+                };
+                cache.writeQuery({ query: GET_TOASTS, data });
+
+                return data.toasts;
+            }
+            return [];
+        },
+    },
+};
