445b1b91761b9bfdb0caa93739dec0d69dc9de38
loverofMush <tolujamal1@gmail.com>
2020-09-22 00:50:06 +0100

    - changed useQuery to uselazyQuery - pass page number to Query on page reload

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useCallback, useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import { GET_CATEGORIES } from '../graphql/query/categories';
 import { AuthConsumer } from '../authContext';
+import DataTable from '../reusables/DataTable';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const Category: React.FC = (): JSX.Element => {
-    const { data, loading, error } = useQuery(GET_CATEGORIES);
+    const [_isMounted, setIsMounted] = useState(true);
+    const history = useHistory();
+
+    const [getCategories, { data, loading, error }] = useLazyQuery(GET_CATEGORIES);
+
+    const items = data?.categories;
+
+    function useUrlQuery(): any {
+        return new URLSearchParams(useLocation().search);
+    }
+    const query = useUrlQuery();
+    const page = query.get('page');
+
+    useEffect(() => {
+        if (_isMounted) {
+            getCategories({ variables: { currentPage: page } });
+        }
+
+        return () => {
+            setIsMounted(false);
+        };
+    }, [_isMounted, getCategories]);
+
+    const changePage = useCallback(
+        (pageNumber: number): void => {
+            if (pageNumber) {
+                getCategories({ variables: { currentPage: pageNumber } });
+                history.push('/categories' + '/' + '?page=' + pageNumber);
+            }
+        },
+        [getCategories, history],
+    );
+
+    useEffect(() => {
+        const ac = new AbortController();
+        if (!page) {
+            return;
+        }
+
+        changePage(page);
+        return function cleanup(): void {
+            ac.abort();
+        };
+    }, [page, changePage]);
 
     if (loading) return <p>...Loading</p>;
     if (error) {
@@ -12,17 +58,17 @@ const Category: React.FC = (): JSX.Element => {
     }
     if (!data) return <p>Not found</p>;
 
-    const categories = () => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return data.categories.map((category: any) => (
-            <div key={category.id}>
-                <p>{category.parent_id}</p>
-                <p>{category.name}</p>
-            </div>
-        ));
-    };
-
-    return <AuthConsumer>{({ user }) => user && categories()}</AuthConsumer>;
+    return (
+        <AuthConsumer>
+            {({ user }) =>
+                user && (items === undefined || items.length < 0) ? (
+                    <h5>No Records Available!</h5>
+                ) : (
+                    <DataTable items={items} changePage={changePage} />
+                )
+            }
+        </AuthConsumer>
+    );
 };
 
 export default Category;
